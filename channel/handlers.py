88ece3a8760510e74c03270e8c4cac859da27c1e
thewitcher745 <thewitcher745@gmail.com>
2025-01-03 04:27:35 +0330

    FEAT: Replaced pair_info with pair_list Simplifying the config

--- channel/handlers.py
+++ channel/handlers.py
@@ -38,23 +38,6 @@ async def handle_add_pair(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     # Separate the setup command and process the inputs
     parts = message_text.split(" ")
 
-    # if len(parts) < 3:
-    #     await context.bot.send_message(chat_id=chat_id, text="❌ Invalid command format. Use /addpair <pair> <timeframe>")
-    #     return
-    #
-    # pair = parts[1]
-    # timeframe = parts[2]
-    #
-    # # Load the current configuration
-    # config = load_config()
-    #
-    # if chat_id not in config:
-    #     config[chat_id] = {
-    #         "posting_interval": 3600
-    #     }
-    #     config[chat_id]["pair_info"] = []
-    # config[chat_id]["pair_info"].append({'pair': pair, 'timeframe': timeframe})
-
     if len(parts) < 2:
         await context.bot.send_message(chat_id=chat_id, text="❌ Invalid command format. Use /addpair <pair>")
         return
@@ -69,8 +52,8 @@ async def handle_add_pair(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         config[chat_id] = {
             "posting_interval": 3600
         }
-        config[chat_id]["pair_info"] = []
-    config[chat_id]["pair_info"].append({'pair': pair})
+        config[chat_id]["pair_list"] = []
+    config[chat_id]["pair_list"].append(pair)
 
     # Save the updated configuration
     save_config(config)
@@ -91,9 +74,8 @@ async def handle_show_pairs(update: Update, context: ContextTypes.DEFAULT_TYPE) 
     config = load_config()
 
     try:
-        for pair_info_item in config[chat_id]["pair_info"]:
-            # message += f"🔹 {pair_info_item['pair']} - {pair_info_item['timeframe']}\n"
-            message += f"🔹 {pair_info_item['pair']}\n"
+        for pair in config[chat_id]["pair_list"]:
+            message += f"🔹 {pair}\n"
     except:
         message = "❌ No data yet."
 
@@ -124,7 +106,7 @@ async def handle_set_posting_interval(update: Update, context: ContextTypes.DEFA
         config[chat_id] = {
             "posting_interval": 3600
         }
-        config[chat_id]["pair_info"] = []
+        config[chat_id]["pair_list"] = []
     config[chat_id]["posting_interval"] = int(interval)
 
     # Save the updated configuration
@@ -138,7 +120,7 @@ async def send_periodic_chart(context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = str(context.job.chat_id)
 
     config = load_config()
-    pair_list = [pair_info_item["pair"].replace("USDT", "").replace("USD", "") for pair_info_item in config[chat_id]["pair_info"]]
+    pair_list = [pair.replace("USDT", "").replace("USD", "") for pair in config[chat_id]["pair_list"]]
 
     # Initialize the Chart class and download the chart
     chart = Chart(pair_list)
